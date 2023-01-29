861f8cde24ba0cc348f3f2795925179e31f1c0a1
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2023-01-29 05:52:41 +0530

    product view updated

--- api/product/views.py
+++ api/product/views.py
@@ -1,10 +1,13 @@
 from rest_framework import viewsets
-
+from rest_framework import generics
 from .serializers import ProductSerializer
 from .models import Product
+from rest_framework import filters
 # Create your views here.
 
 
 class ProductViewSet(viewsets.ModelViewSet):
+    search_fields = ['name']
+    filter_backends = (filters.SearchFilter,)
     queryset = Product.objects.all().order_by('id')
     serializer_class = ProductSerializer
